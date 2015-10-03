3f1430a4a5b6e63bc4832359c82730961eca3516
Ryan Yep <ryan.yep@gmail.com>
2015-10-03 15:53:53 -0500

    Add navbar with linnks

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get     '/login',  to: 'sessions#new',     as: 'new_login'
   post    '/login',  to: 'sessions#create',  as: 'login'
   delete  '/logout', to: 'sessions#destroy', as: 'logout'
-  
+
+  root 'home#index'
+
 end
