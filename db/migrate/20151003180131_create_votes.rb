5fe0928baef1e6577d4a98f396bf5723c8ae46cc
Ryan Yep <ryan.yep@gmail.com>
2015-10-03 13:05:21 -0500

    migrate & set default vote_count to 0

--- db/migrate/20151003180131_create_votes.rb
+++ db/migrate/20151003180131_create_votes.rb
@@ -1,7 +1,7 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.int :vote_count
+      t.integer :vote_count, default: 0
       t.belongs_to :link, index: true, foreign_key: true
 
       t.timestamps null: false
