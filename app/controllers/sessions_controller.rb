7f37d49168eec4396d0aaee14d015f4932444ab3
Ryan Yep <ryan.yep@gmail.com>
2015-10-03 15:15:25 -0500

    Add sessions_controller, application_controller and session view

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  skip_before_filter :authenticate_user
+
+  def new #login page
+  end
+
+  def create #create the login session
+    user = User.find_by_username(params[:username])
+    if user.present? && user.authenticate(params[:password])
+      set_user_session(user)
+      redirect_to user_path(user), notice: "Successfully logged in."
+    else
+      flash[:alert] = "Username or Password did not match."
+      render :new
+    end
+
+    def destroy #delete the login session
+      session[:logged_in_user] = nil
+      redirect_to root_path, notice: "Successfully logged out."
+    end
+
+    private
+
+    def set_user_session(user) #places temporary cookie on user's browser
+      session[:logged_in_user] = user.id
+    end
+  end
+end
