c8c30117adb243670e9a2db35119fe5f27c90a06
Ryan Yep <ryan.yep@gmail.com>
2015-10-03 13:21:36 -0500

    Add dependent destroy to user and link models

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,4 @@
 class Link < ActiveRecord::Base
   belongs_to :user
-  has_many   :votes
+  has_many   :votes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :links
+  has_many :links, dependent: :destroy
+  has_many :votes, through: :links
   has_secure_password
 end
