e999ad8d91ed00d29de672f1de044ba1c3356af3
Ryan Yep <ryan.yep@gmail.com>
2015-10-12 15:44:07 -0500

    Updated controllers with appropriate before_action and retrieval of flash messages in application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-  skip_before_filter :authenticate_user
 
   def new #login page
   end
@@ -7,8 +6,8 @@ class SessionsController < ApplicationController
   def create #create the login session
     user = User.find_by_username(params[:username])
     if user.present? && user.authenticate(params[:password])
-      set_user_session(user)
-      redirect_to user_path(user), notice: "Successfully logged in."
+      session[:logged_in_user] = user.id
+      redirect_to user, notice: "Successfully logged in."
     else
       flash[:alert] = "Username or Password did not match."
       render :new
@@ -20,9 +19,4 @@ class SessionsController < ApplicationController
     redirect_to root_path, notice: "Successfully logged out."
   end
 
-  private
-
-    def set_user_session(user) #places temporary cookie on user's browser
-      session[:logged_in_user] = user.id
-    end
 end
